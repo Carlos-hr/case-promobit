66d285de92031f877d75bb85203b61d51f69adf5
Carlos Henrique Rodrigues da Silva <carlos.silva@pampa.compasso>
2021-09-09 10:57:28 -0300

    Setup inicial, componentização e definição de páginas MVP

---
+++ promobit/src/pages/DetailsPage/DetailsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+export const DetailsPage = () => {
+	return (
+		<div>
+			<Header />
+			<h1> Eu sou a DetailsPage</h1>
+		</div>
+	);
+};
+
+export default DetailsPage;
--- promobit/src/routes/Router.js
+++ promobit/src/routes/Router.js
@@ -1,18 +1,32 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import HomePage from "../pages/HomePage/HomePage";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage';
+import MoviesPage from '../pages/MoviesPage/MoviesPage';
+import DetailsPage from '../pages/DetailsPage/DetailsPage';
+import ErrorPage from '../pages/ErrorPage';
 
 export const Router = () => {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/">
+					<HomePage />
+				</Route>
 
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path='/'>
-                    <HomePage/>
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    )
-}
+				<Route exact path="/movies/:id">
+					<DetailsPage />
+				</Route>
 
-export default Router
+				<Route exact path="/movies">
+					<MoviesPage />
+				</Route>
+
+				<Route>
+					<ErrorPage />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
+};
+
+export default Router;

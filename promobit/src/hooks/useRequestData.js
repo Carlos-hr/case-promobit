872fccbf761adc7743fb4aa8a410e14e56f0c18f
Carlos Henrique Rodrigues da Silva <carlos.silva@pampa.compasso>
2021-11-12 19:53:11 -0300

    Adding dotenv

--- promobit/src/hooks/useRequestData.js
+++ promobit/src/hooks/useRequestData.js
@@ -1,37 +1,40 @@
-import { useEffect, useState, useCallback } from 'react';
-import axios from 'axios';
-import { API_KEY } from '../constants/urls';
+import { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const useRequestData = (url, initialState, page, query) => {
-	if (!page) {
-		page = 1;
-	}
-	if (!query) {
-		query = '';
-	}
-	const [ data, setData ] = useState(initialState);
-	const getData = useCallback(
-		() => {
-			axios
-				.get(url, { params: { api_key: API_KEY, language: 'pt-BR', page: Number(page), query } })
-				.then((res) => {
-					setData(res.data);
-				})
-				.catch((err) => {
-					setData(false);
-				});
-		},
-		[ page, query, url ]
-	);
+  if (!page) {
+    page = 1;
+  }
+  if (!query) {
+    query = "";
+  }
+  const [data, setData] = useState(initialState);
+  const getData = useCallback(() => {
+    axios
+      .get(url, {
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+          language: "pt-BR",
+          page: Number(page),
+          query,
+        },
+      })
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        setData(false);
+      });
+  }, [page, query, url]);
 
-	useEffect(
-		() => {
-			getData();
-		},
-		[ getData, url ]
-	);
+  useEffect(() => {
+    getData();
+  }, [getData, url]);
 
-	return { data, getData };
+  return { data, getData };
 };
 
 export default useRequestData;
